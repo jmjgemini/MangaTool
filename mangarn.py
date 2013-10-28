1823d1b22818d12d15d9d0100aca8a416421d452
jmjgemini <jmjgeminix@gmail.com>
2013-10-28 23:21:20 +0800

    modified:   mangarn.py

--- mangarn.py
+++ mangarn.py
@@ -5,9 +5,13 @@ import string
 
 def change_name(file,level):
 	if os.path.isfile(file):
-		lists = file.split('.')
-		file_name= lists[0]
-		file_ext = lists[1]
+		#deal with file name
+		file_list = file.split('.')
+		file_list_len = len(file_list)
+		file_ext = file_list[file_list_len -1]
+		file_name = file_list[0:file_list_len-2]
+
+
 		if file_ext in img_ext:
 			i = string.atoi(file_name)
 			new_file_name = '%03d'%i
