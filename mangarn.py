68cda0ee25314ac045dd51c952a681773763c704
jmjgemini <jmjgeminix@gmail.com>
2013-10-28 18:36:34 +0800

    new file:   README.md 	new file:   mangarn.py

---
+++ mangarn.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import os
+import string
+
+def change_name(file,level):
+	if os.path.isfile(file):
+		lists = file.split('.')
+		file_name= lists[0]
+		file_ext = lists[1]
+		if file_ext in img_ext:
+			i = string.atoi(file_name)
+			new_filename = '%03d'%i
+			newfile = new_filename +'.'+ file_ext
+			print 'changeing '+file+' to '+ newfile
+			os.rename(file,newfile)
+
+
+			
+		
+img_ext = ['jpg','jpeg','png']
+dir = os.getcwd()
+files = os.listdir(dir)
+level = 3
+
+for file in files:
+	change_name(file,level)
+
+
