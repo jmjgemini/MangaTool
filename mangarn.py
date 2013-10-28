ce3a1469e433a6005b927fc9f421a10e5322e80d
jmjgemini <jmjgeminix@gmail.com>
2013-10-29 00:19:52 +0800

    modified:   mangarn.py

--- mangarn.py
+++ mangarn.py
@@ -2,6 +2,19 @@
 
 import os
 import string
+import sys
+import getopt
+
+#default settings
+appname = 'mangarn'
+img_ext = ['jpg','jpeg','png']
+
+show_infomation = True #contorls if the program 
+			#show infomation to stdout
+level = 3              #example level = 3 then 1.jpg => 001.jpg
+
+def usage():
+	print 'usage:%s [-hsl] [arg]'%appname
 
 def change_name(file,level):
 	if os.path.isfile(file):
@@ -9,25 +22,43 @@ def change_name(file,level):
 		file_list = file.split('.')
 		file_list_len = len(file_list)
 		file_ext = file_list[file_list_len -1]
-		file_name = file_list[0:file_list_len-2]
+		file_name = file_list[0]
 
-
+		#deal with the picture
 		if file_ext in img_ext:
 			i = string.atoi(file_name)
-			new_file_name = '%03d'%i
+			new_file_name = ('%%0%dd'%level)%i
 			newfile = new_file_name +'.'+ file_ext
-			print 'changeing '+file+' to '+ newfile
+			if show_infomation:
+				print 	'{0: >10}'.format(file) +\
+					' => '+\
+					'{0: >10}'.format(newfile)
 			os.rename(file,newfile)
 
 
 			
-		
-img_ext = ['jpg','jpeg','png']
-dir = os.getcwd()
-files = os.listdir(dir)
-level = 3
+def main(argv):
 
-for file in files:
-	change_name(file,level)
+	#deal with opts
+	try:
+		opts,args =\
+		getopt.getopt(argv,'hsl:',['help','silent','level='])
+	except getopt.GetoptError:
+		usage()
+		sys.exit(2)
+	for opt,arg in opts:
+		if opt in ("-h","--help"):
+			usage()
+			sys.exit()
+		elif opt in ('-s','--silent'):
+			show_infomation = False
+		elif opt in ('-l','--level'):
+			level = string.atoi(arg)
+	
+	dir = os.getcwd()
+	files = os.listdir(dir)
+	for file in files:
+		change_name(file,level)
 
-
+if __name__ == "__main__":
+	main(sys.argv[1:])
